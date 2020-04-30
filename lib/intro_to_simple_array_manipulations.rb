01f7b7681c010db4136070ceff00763a5638c2b0
Jonathan Reid <iamjonathanreid@gmail.com>
2020-04-30 08:56:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,7 +14,11 @@ def using_flatten(array)
   array.flatten 
 end
 
-def using_delete(array, n)
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, n)
   array.delete_at(n)
- end 
+end 
 
